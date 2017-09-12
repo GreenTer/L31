253062862103127feafa57ce0af2c2060ad61ac0
GreenTer <thenewlife@tut.by>
2017-09-13 01:24:48 +0300

    Test

--- app2.rb
+++ app2.rb
@@ -10,6 +10,13 @@ loop do
 	x = hh[id].to_i 	# читает хеш value (0 если если не существует)
 	x = x + n 			# увеличить на n
 	hh[id] = x 			# добавить в хеш value
-	
+
 	puts hh.inspect
+	puts '================='
+
+	hh.each do |key, value| 	# посчитать общее колличество добавленных вещей в корзину
+		total = total + value
+	end
+
+	puts "Total items in cart: #{total}"
 end
