117973e51b7d62c78c7c3aa2b91fa3171ee3f261
GreenTer <thenewlife@tut.by>
2017-09-13 00:27:28 +0300

    Добавил id add_to_cart

--- public/app.js
+++ public/app.js
@@ -1,10 +1,15 @@
+function add_to_cart(id)
+{
+	alert('You added pizza with :id' + id);
+}
+
 function something()
 {
-	var x = window.localStorage.getItem('aaa');
+  var x = window.localStorage.getItem('aaa');
 
-	x = x * 1 + 1;
+  x = x * 1 + 1;
 
-	window.localStorage.setItem('aaa', x)
+  window.localStorage.setItem('aaa', x);
 
-	alert(x);
+  alert(x);
 }
