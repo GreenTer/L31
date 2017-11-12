4d06ada1108331407c7afb7ecba975ab407c21c6
GreenTer <thenewlife@tut.by>
2017-11-12 16:20:11 +0300

    Добавили обработчик заказов

--- app.rb
+++ app.rb
@@ -14,10 +14,34 @@ get '/' do
 	erb :index
 end
 
-post '/cart' do
-	erb :cart
-end
-
 get '/about' do
 	erb :about
 end
+
+post '/cart' do
+	orders_input = params[:orders]
+	@orders = parse_orders_input orders_input
+	erb :cart
+end
+
+def parse_orders_input orders_input
+
+	s1 = orders_input.split(/,/)
+
+	arr = []
+
+	s1.each do |x|
+		s2 = x.split(/=/)
+		s3 = s2[0].split(/_/)
+
+		id = s3[1]
+		cnt = s2[1]
+		
+		arr2 = [id, cnt]
+		arr.push arr2
+	end
+
+	return arr
+end
+
+puts parse_orders_line(orders_line).inspect
--- app3.rb
+++ app3.rb
@@ -1,15 +1,23 @@
-orders = "product_1=4,product_2=3,product_3=4,"
+orders_line = "product_1=4,product_2=3,product_3=4,"
 
-s1 = orders.split(/,/)
+def parse_orders_line orders_line
 
-puts s1.inspect
+	s1 = orders_line.split(/,/)
 
-s1.each do |x|
-	s2 = x.split(/=/)
-	puts s2.inspect
+	arr = []
 
-	s3 = s2[0].split(/_/)
-	puts s3.inspect
+	s1.each do |x|
+		s2 = x.split(/=/)
+		s3 = s2[0].split(/_/)
 
-	puts "Пицца: #{s3[1]}, колличество #{s2[1]}"
+		id = s3[1]
+		cnt = s2[1]
+		
+		arr2 = [id, cnt]
+		arr.push arr2
+	end
+
+	return arr
 end
+
+puts parse_orders_line(orders_line).inspect
